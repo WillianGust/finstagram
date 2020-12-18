8334b8ca1fc972ad8179c9fd7dc94fd4f111307d
WillianGust <williandes.silva@yahoo.ca>
2020-12-18 01:52:19 +0000

    pre-deployment

--- app/actions.rb
+++ app/actions.rb
@@ -1,51 +1,118 @@
-def humanized_time_ago(time_ago_in_minutes)
-  if time_ago_in_minutes >= 60
-    "#{time_ago_in_minutes / 60} hours ago"
-  else
-    "#{time_ago_in_minutes} minutes ago"
+helpers do 
+  def current_user
+    User.find_by(id: session[:user_id]) # allow us to access the currently logged in user in any ERB file.
+  end
+  def logged_in?
+    !!current_user
   end
 end
 
 get '/' do
-  @finstagram_post_shark = {
-    username: "sharky_j",
-    avatar_url: "http://naserca.com/images/sharky_j.jpg",
-    photo_url: "http://naserca.com/images/shark.jpg",
-    humanized_time_ago: humanized_time_ago(15),
-    like_count: 0,
-    comment_count: 1, 
-    comments: [{
-      username: "sharky_j",
-      text: "Out for the long weekend... too embarrassed to show y'all the beach bod!"
-    }]
-  }
-  @finstagram_post_whale = {
-    username: "kirk_whalum",
-    avatar_url: "http://naserca.com/images/kirk_whalum.jpg",
-    photo_url: "http://naserca.com/images/whale.jpg",
-    humanized_time_ago: humanized_time_ago(65),
-    like_count: 0,
-    comment_count: 1,
-    comments: [{
-      username: "kirk_whalum",
-      text: "#weekendvibes"
-    }]
-  }
-
-  @finstagram_post_marlin = {
-    username: "marlin_peppa",
-    avatar_url: "http://naserca.com/images/marlin_peppa.jpg",
-    photo_url: "http://naserca.com/images/marlin.jpg",
-    humanized_time_ago: humanized_time_ago(190),
-    like_count: 0,
-    comment_count: 1,
-    comments: [{
-      username: "marlin_peppa",
-      text: "lunchtime! ;)"
-    }]
-  }
-
-  @finstagram_posts = [@finstagram_post_shark, @finstagram_post_whale, @finstagram_post_marlin]
-
+  @finstagram_posts = FinstagramPost.order(created_at: :desc)
   erb(:index)
+end
+
+# signup form
+post '/signup' do
+  email = params[:email]
+  avatar_url = params[:avatar_url]
+  username = params[:username]
+  password = params[:password]
+  @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
+  if @user.save
+    redirect to('/login')
+  else
+    erb(:signup)
+  end
+end
+
+# signup a new user
+get '/signup' do     
+  @user = User.new   
+  erb(:signup)       
+end
+
+get '/login' do
+  erb(:login)
+end
+
+post '/login' do
+  username = params[:username]
+  password = params[:password]
+  user = User.find_by(username: username)  
+  if user && user.password == password
+    session[:user_id] = user.id
+    redirect to('/')
+  else
+    @error_message = "Login failed."
+    erb(:login)
+  end
+end
+# display the user's name in the header on the homepage
+get '/' do
+    @finstagram_post = FinstagramPost.order(created_at: desc)
+    current_user = User.find_by(id: session[:user_id])
+    erb(index)
+end
+
+get '/logout' do
+  session[:user_id] = nil
+  redirect to('/')
+end
+
+# create new post
+post '/finstagram_posts' do
+  photo_url = params[:photo_url]
+  @finstagram_post = FinstagramPost.new({ photo_url: photo_url, user_id: current_user.id })
+  if @finstagram_post.save
+    redirect(to('/'))
+  else
+    erb(:"finstagram_posts/new")
+  end
+end
+
+before '/finstagram_posts/new' do
+  redirect to('/login') unless logged_in?
+end
+
+# form to create new post
+get '/finstagram_posts/new' do
+  @finstagram_post = FinstagramPost.new
+  erb(:"finstagram_posts/new")
+end
+
+# show a single post
+get '/finstagram_posts/:id' do
+  @finstagram_post = FinstagramPost.find(params[:id])
+  erb(:"finstagram_posts/show")
+end 
+
+post '/comments' do
+  # point values from params to variables
+  text = params[:text]
+  finstagram_post_id = params[:finstagram_post_id]
+
+  # instantiate a comment with those values & assign the comment to the `current_user`
+  comment = Comment.new({ text: text, finstagram_post_id: finstagram_post_id, user_id: current_user.id })
+
+  # save the comment
+  comment.save
+
+  # `redirect` back to wherever we came from
+  redirect(back)
+end
+
+post '/likes' do
+  finstagram_post_id = params[:finstagram_post_id]
+
+  like = Like.new({ finstagram_post_id: finstagram_post_id, user_id: current_user.id })
+  like.save
+
+  redirect(back)
+end
+
+delete '/likes/:id' do
+  like = Like.find(params[:id])
+  like.destroy
+  redirect(back)
 end
